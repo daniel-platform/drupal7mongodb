379cf392a0b2ae89c5a9ad713a0814b873a91db0
Daniel Morante <daniel@platform.sh>
2015-11-03 15:23:46 -0500

    Debug

--- settings.php
+++ settings.php
@@ -13,11 +13,12 @@ if (!empty($relationships['mongodb'][0])) {
 	$mongodb_connection = json_decode(base64_decode(getenv('PLATFORM_RELATIONSHIPS')))->mongodb[0];
 	
     // Omit USER:PASS@ if Mongo isn't configured to use authentication.
-	$mongo_db_url = sprintf('mongodb://%s:%s@%s:%s',  
+	$mongo_db_url = sprintf('mongodb://%s:%s@%s:%s/%s',  
 		$mongodb_connection->username,
 		$mongodb_connection->password,
 		$mongodb_connection->host,
-		$mongodb_connection->port
+		$mongodb_connection->port,
+		$mongodb_connection->path
 	);
 	
     $conf['mongodb_connections'] = array(
